8cc413b814d03289eff9556e38ca4afe979ca687
LCD <skillpb123@gmail.com>
2021-10-26 16:09:08 +0700

    set up environment variables, set up ESlint + Prettier

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const morgan = require('morgan');
+
 const app = express();
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 // 1) middleware
+
+console.log(` 
+************************************
+--- our environment: ${process.env.NODE_ENV}
+--- Author: ${process.env.USER_NAME}
+************************************
+`);
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(express.static(`${__dirname}/public`));
+
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,6 +2,27 @@ const fs = require('fs');
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
 
+exports.checkId = (req, res, next, val) => {
+    console.log(`tour id: ${val}`);
+    if (req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID',
+        });
+    }
+    next();
+};
+
+exports.checkBody = (req, res, next) => {
+    if (!req.body.name || !req.body.price) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Missing name or price',
+        });
+    }
+    next();
+};
+
 exports.getAllTours = function (req, res) {
     console.log(req.requestTime);
     res.status(200).json({
@@ -19,12 +40,6 @@ exports.getTour = function (req, res) {
 
     const tour = tours.find((el) => el.id === id);
 
-    if (!tour) {
-        return res.status(404).json({
-            status: 'error',
-            message: 'Invalid ID',
-        });
-    }
     res.status(200).json({
         status: 'success',
         results: tours.length,
@@ -39,12 +54,6 @@ exports.updateTour = function (req, res) {
 
     const tour = tours.find((el) => el.id === id);
 
-    if (!tour) {
-        return res.status(404).json({
-            status: 'error',
-            message: 'Invalid ID',
-        });
-    }
     res.status(200).json({
         status: 'success',
         results: tours.length,
@@ -74,12 +83,6 @@ exports.deleteTour = function (req, res) {
 
     const tour = tours.find((el) => el.id === id);
 
-    if (!tour) {
-        return res.status(404).json({
-            status: 'error',
-            message: 'Invalid ID for delete',
-        });
-    }
     // delete tour
     res.status(200).json({
         status: 'success',
